02bebbfbf5c45163bbae5581a5d50ac0a4f8ac2e
teiling88 <teiling88@gmail.com>
2025-09-24 13:46:36 +0200

    add tools and create unit test setup

---
+++ .php-cs-fixer.php
@@ -0,0 +1,66 @@
+<?php
+use PhpCsFixer\Config;
+
+$blacklistedFiles = [
+    'credentials.php',
+];
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/examples')
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->filter(function(SplFileInfo $file) use ($blacklistedFiles) {
+        return !in_array($file->getFilename(), $blacklistedFiles, true);
+    })
+;
+
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+
+        // Fix declare style
+        'blank_line_after_opening_tag' => false,
+
+        // override @Symonfy
+        'phpdoc_align' => false,
+        'phpdoc_separation' => false,
+        'yoda_style' => false,
+        'phpdoc_summary' => false,
+        'increment_style' => false,
+        'php_unit_fqcn_annotation' => false,
+
+        'array_syntax' => [
+            'syntax' => 'short'
+        ],
+        'class_definition' => [
+            'single_line' => true
+        ],
+        'comment_to_phpdoc' => true,
+        'concat_space' => [
+            'spacing' => 'one'
+        ],
+        'declare_strict_types' => true,
+        'dir_constant' => true,
+        'is_null' => true,
+        'no_null_property_initialization' => true,
+        'no_superfluous_phpdoc_tags' => true,
+        'no_useless_return' => true,
+        'no_useless_else' => true,
+        'multiline_whitespace_before_semicolons' => true,
+        'mb_str_functions' => true,
+        'ordered_class_elements' => false,
+        'ordered_imports' => true,
+        'phpdoc_order_by_value' => true,
+        'php_unit_namespaced' => true,
+        'php_unit_construct' => true,
+        'phpdoc_add_missing_param_annotation' => [
+            'only_untyped' => true
+        ],
+        'phpdoc_order' => true,
+        'phpdoc_var_annotation_correct_order' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
---
+++ Makefile
@@ -0,0 +1,34 @@
+MAKEFLAGS += --warn-undefined-variables
+MAKEFLAGS += --no-builtin-rules
+
+.PHONY: help
+.PHONY: help
+help:       ## Shows the help
+help:
+	@printf "\033[33mUsage:\033[0m\n  make TARGET\n\n\033[32m#\n# Commands\n#---------------------------------------------------------------------------\033[0m\n"
+	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//' | awk 'BEGIN {FS = ":"}; {printf "\033[33m%s:\033[0m%s\n", $$1, $$2}'
+
+.PHONY: ddev-quality
+ddev-quality:     ## executes all quality scripts in the running ddev container
+ddev-quality:
+	ddev exec -s web "PHP_CS_FIXER_IGNORE_ENV=true ./tools/php-cs-fixer fix --config=.php-cs-fixer.php"
+	ddev exec -s web "./tools/phpstan analyse -l 5 ./src/"
+	ddev exec -s web "./vendor/bin/phpunit --stop-on-failure --stop-on-error --coverage-html=./build/artifacts/html-coverage"
+
+
+.PHONY: ddev-cs
+ddev-cs:     ## executes code style scripts in the running ddev container
+ddev-cs:
+	ddev exec -s web "PHP_CS_FIXER_IGNORE_ENV=true ./tools/php-cs-fixer fix --config=.php-cs-fixer.php"
+
+.PHONY: ddev-phpstan
+ddev-phpstan:     ## executes phpstan scripts in the running ddev container
+ddev-phpstan:
+	ddev exec -s web "./tools/phpstan analyse -l 5 ./src/"
+
+.PHONY: ddev-unit
+ddev-unit:     ## executes php unit in the running ddev container
+ddev-unit:
+	ddev exec -s web "./vendor/bin/phpunit --stop-on-failure --stop-on-error --coverage-html=./build/artifacts/html-coverage"
+
+
